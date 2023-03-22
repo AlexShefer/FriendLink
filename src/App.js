dbe2de327c7daf28b0d1ca152ca78b7b117d1394
Aleksandr Shefer <avshef451987@gmail.com>
2023-03-22 19:02:26 +0500

    Register and Login page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Navbar from "scenes/navbar";
 function App() {
     const mode = useSelector((state) => state.mode);
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    const isAuth = Boolean(useSelector((state) => state.token));
     return (
         <div className="app">
             <BrowserRouter>
@@ -19,10 +20,17 @@ function App() {
                     <CssBaseline />
                     <Routes>
                         <Route path="/" element={<LoginPage />} />
-                        <Route path="/home" element={<HomePage />} />
+                        <Route
+                            path="/home"
+                            element={
+                                isAuth ? <HomePage /> : <Navigate to="/" />
+                            }
+                        />
                         <Route
                             path="/profile/:userId"
-                            element={<ProfilePage />}
+                            element={
+                                isAuth ? <HomePage /> : <Navigate to="/" />
+                            }
                         />
                     </Routes>
                 </ThemeProvider>
--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -39,7 +39,7 @@ function Navbar() {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    const fullName = user ? `${user.firstName} ${user.lastName}` : "noname";
+    const fullName = `${user.firstName} ${user.lastName}`;
 
     return (
         <FlexBetween padding="1rem 6%" backgroundColor={alt}>
